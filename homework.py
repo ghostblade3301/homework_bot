7a130de1928be6a32e7c72051892d9b6f0d6d528
Pavel Naumenko <pavelnaumenkowork@gmail.com>
2023-03-07 15:36:17 +0300

    few changes

--- homework.py
+++ homework.py
@@ -1,11 +1,15 @@
-...
+import os
+import logging
+
+from dotenv import load_dotenv
+
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -18,33 +22,40 @@ HOMEWORK_VERDICTS = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
 
 def check_tokens():
-    ...
+    """Проверка наличия токенов."""
+    if not PRACTICUM_TOKEN:
+        logging
+    pass
 
 
 def send_message(bot, message):
-    ...
+    """Отправка сообщения в телеграм."""
+    pass
 
 
 def get_api_answer(timestamp):
-    ...
+    """Получение ответа от api."""
+    pass
 
 
 def check_response(response):
-    ...
+    """Проверка ответа."""
+    pass
 
 
 def parse_status(homework):
-    ...
-
+    """Проверка статуса работы."""
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def main():
     """Основная логика работы бота."""
-
-    ...
+    pass
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
