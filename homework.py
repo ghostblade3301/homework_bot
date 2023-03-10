87f1353031f49e7abfb67562d1b7232bc4db2ca2
Pavel Naumenko <ghostblade3301@gmail.com>
2023-03-10 21:49:29 +0300

    added logging

--- homework.py
+++ homework.py
@@ -15,7 +15,7 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_PERIOD = 10
+RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
@@ -126,4 +126,9 @@ def main():
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s, [%(levelname)s] %(message)s',
+        stream=sys.stdout,
+    )
     main()
